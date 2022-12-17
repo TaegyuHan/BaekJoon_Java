8782d2d50c66aac5a7b123bdcf5ea8bbe34fe5f3
한태규 <gksxorb147@gmail.com>
2022-12-17 16:57:29 +0900

    해결 날짜: 2022-12-17 사이트: 백준 문제: 시작 예시 번호: --

---
+++ src/main/java/start/Main.java
@@ -0,0 +1,12 @@
+package start;
+
+
+/**
+ * Solution code for "BaekJoon 문제이름".
+ * <p>
+ * - Problem link: <a href="https://www.acmicpc.net/problem/번호입력">Link</a>
+ * 작성자: gksxorb159
+ * BigO: O(2N) : 100_000 * 2 = 200_000
+ */
+public class Main {
+}
