2f9ebf5f187eef503244e25ef631f951766762fc
한태규 <gksxorb147@gmail.com>
2023-04-04 01:22:47 +0900

    초기 세팅 정리

---
+++ src/main/java/queue/Path.java
@@ -0,0 +1,5 @@
+package queue;
+
+public class Path {
+    public final static String QUEUE_PATH = "./src/main/java/queue";
+}
--- src/main/java/start/Main.java
+++ src/main/java/start/Main.java
@@ -1,12 +1,23 @@
 package start;
 
+import java.io.*;
 
 /**
- * Solution code for "BaekJoon 문제이름".
+ * Solution code for "BaekJoon 문제".
  * <p>
- * - Problem link: <a href="https://www.acmicpc.net/problem/문제번호">Link</a>
+ * - Problem link: <a href="https://www.acmicpc.net/problem/숫자">Link</a>
  * 작성자: gksxorb147
  * BigO: O() :
  */
 public class Main {
+
+    private final static String TEST_PATH = "/number/input/1.txt";
+
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("" + TEST_PATH));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        br.close();
+    }
 }
