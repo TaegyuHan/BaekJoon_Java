5091becb8b5ec50fc2fc3de4357ac9ab847e8d04
Taegyu <59700057+TaegyuHan@users.noreply.github.com>
2023-07-17 11:05:29 +0900

    해결 날짜: 2023-07-17 백준문제 [구현] 2740 행렬 곱셈

--- src/main/java/implement/p2740/Main.java
+++ src/main/java/implement/p2740/Main.java
@@ -1,7 +1,6 @@
 package implement.p2740;
 
 import java.io.*;
-import java.util.StringTokenizer;
 
 import static implement.Path.IMPLEMENT_PATH;
 
@@ -12,67 +11,101 @@ import static implement.Path.IMPLEMENT_PATH;
  * 작성자: gksxorb159
  * BigO: O(N^3 + 2N) :
  */
+class Matrix {
+    private final int row;
+    private final int col;
+    private final int[][] metrix;
+    public Matrix(int row, int col) {
+        this.row = row;
+        this.col = col;
+        this.metrix = new int[row][col];
+    }
+
+    public Matrix(int[][] metrix) {
+        this.row = metrix.length;
+        this.col = metrix[0].length;
+        this.metrix = metrix;
+    }
+
+    public int getMetricsData(int row, int col) {
+        return  metrix[row][col];
+    }
+
+    public int getRow() {
+        return this.row;
+    }
+
+    public int getCol() {
+        return this.col;
+    }
+
+    public void setRow(int rowIndex, String row) {
+        String[] rowData = row.split(" ");
+        for (int i = 0; i < col; i++) {
+            metrix[rowIndex][i] = Integer.parseInt(rowData[i]);
+        }
+    }
+
+    public Matrix multiplication(Matrix other) throws Exception {
+
+        if (col != other.getRow()) {
+            throw new Exception("배열의 길이가 다릅니다.");
+        }
+
+        int[][] newMetrics = new int[row][other.getCol()];
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < other.getCol(); j++) {
+                for (int k = 0; k < col; k++) {
+                    newMetrics[i][j] += metrix[i][k] * other.getMetricsData(k, j);
+                }
+            }
+        }
+
+        return new Matrix(newMetrics);
+    }
+
+    public void showMatrix() {
+        for (int r = 0; r < row; r++) {
+            for (int c = 0; c < col; c++) {
+                System.out.print(metrix[r][c] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
 public class Main {
 
-    public final static String TEST_PATH = "/p2740/input/3.txt";
+    public final static String TEST_PATH = "/p2740/input/1.txt";
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         System.setIn(new FileInputStream(IMPLEMENT_PATH + TEST_PATH));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = inputLine(br);
 
-        // 첫번째 행렬
-        int row1 = inputInt(st);
-        int col1 = inputInt(st);
-        int[][] board1 = new int[row1][col1];
-        for (int i = 0; i < row1; i++) {
-            st = inputLine(br);
-            for (int j = 0; j < col1; j++) {
-                board1[i][j] = inputInt(st);
-            }
+        String[] Adata = br.readLine().split(" ");
+        int Arow = Integer.parseInt(Adata[0]);
+        int Acol = Integer.parseInt(Adata[1]);
+
+        Matrix A = new Matrix(Arow, Acol);
+
+        for (int i = 0; i < Arow; i++) {
+            A.setRow(i, br.readLine());
         }
 
-        // 두번째 행렬
-        st = inputLine(br);
+        String[] Bdata = br.readLine().split(" ");
+        int Brow = Integer.parseInt(Bdata[0]);
+        int Bcol = Integer.parseInt(Bdata[1]);
 
-        int row2 = inputInt(st);
-        int col2 = inputInt(st);
-        int[][] board2 = new int[row2][col2];
-        for (int i = 0; i < row2; i++) {
-            st = inputLine(br);
-            for (int j = 0; j < col2; j++) {
-                board2[i][j] = inputInt(st);
-            }
+        Matrix B = new Matrix(Brow, Bcol);
+
+        for (int i = 0; i < Brow; i++) {
+            B.setRow(i, br.readLine());
         }
+
         br.close();
 
-        // 행렬 덧셈
-        StringBuilder sb = new StringBuilder();
-        for (int x1 = 0; x1 < row1; x1++) {
-            for (int r1 = 0; r1 < col2; r1++) {
-                int tmp = 0;
-                for (int y1 = 0; y1 < col1; y1++) {
-                    tmp += board1[x1][y1] * board2[y1][r1];
-//                    System.out.printf("%d %d %d %d\n", x1, y1, y1, r1);
-                }
-                sb.append(tmp);
-                if (r1 == col2 - 1 && x1 != row1 - 1) {
-                    sb.append("\n");
-                } else {
-                    sb.append(" ");
-                }
-            }
-        }
-        System.out.println(sb);
-    }
-
-    public static StringTokenizer inputLine(BufferedReader br) throws IOException {
-        // 1라인 데이터 받기
-        return new StringTokenizer(br.readLine());
-    }
-
-    public static int inputInt(StringTokenizer st) {
-        // 데이터 숫자 받기
-        return Integer.parseInt(st.nextToken());
+        Matrix C = A.multiplication(B);
+        C.showMatrix();
     }
 }
