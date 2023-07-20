caa37fa8b6dfea5207c91cbec237ff97fa5ce487
Taegyu <59700057+TaegyuHan@users.noreply.github.com>
2023-07-20 11:34:18 +0900

    해결 날짜: 2023-07-20 README 생성기

---
+++ src/main/java/ReadmeMake.java
@@ -0,0 +1,42 @@
+public class ReadmeMake {
+
+    private static final int BRONZE_5 = 1;
+    private static final int BRONZE_4 = 2;
+    private static final int BRONZE_3 = 3;
+    private static final int BRONZE_2 = 4;
+    private static final int BRONZE_1 = 5;
+    private static final int SILVER_5 = 6;
+    private static final int SILVER_4 = 7;
+    private static final int SILVER_3 = 8;
+    private static final int SILVER_2 = 9;
+    private static final int SILVER_1 = 10;
+    private static final int GOLD_5 = 11;
+    private static final int GOLD_4 = 12;
+    private static final int GOLD_3 = 13;
+    private static final int GOLD_2 = 14;
+    private static final int GOLD_1 = 15;
+    private static final int PLATINUM_5 = 16;
+    private static final int PLATINUM_4 = 17;
+    private static final int PLATINUM_3 = 18;
+    private static final int PLATINUM_2 = 19;
+    private static final int PLATINUM_1 = 20;
+    private static final int DIAMOND_5 = 21;
+    private static final int DIAMOND_4 = 22;
+    private static final int DIAMOND_3 = 23;
+    private static final int DIAMOND_2 = 24;
+    private static final int DIAMOND_1 = 25;
+    private static final int RUBY_5 = 26;
+    private static final int RUBY_4 = 27;
+    private static final int RUBY_3 = 28;
+    private static final int RUBY_2 = 29;
+    private static final int RUBY_1 = 30;
+
+    public static void main(String[] args) {
+
+        int rank = GOLD_1;
+        int number = 11505;
+        String name = "구간 곱 구하기";
+
+        System.out.println("| <img height=\"20px\" width=\"20px\" src=\"https://static.solved.ac/tier_small/" + rank +".svg\"/> | [" + number + "](https://www.acmicpc.net/problem/" + number +") | [" + name + "](https://www.acmicpc.net/problem/" + number + ") | [바로가기](./p" + number + "/Main.java) |");
+    }
+}
